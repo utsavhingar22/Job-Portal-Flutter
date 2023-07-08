eb8dd4b1e3bd8bdf6490638c3fd3d06d18b36b53
Utsav Hingar <67818724+utsavhingar22@users.noreply.github.com>
2023-07-08 23:48:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:flutter_job_portal/ui/home_page.dart';
+import 'package:flutter_job_portal/ui/profile_page.dart';
 
 void main() {
   runApp(MyApp());
